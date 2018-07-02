9a96487f46996b9cb0946c7acadb123a612c85a7
nataliadiadkova <nataliadiadkova@gmail.com>
2018-07-02 11:27:44 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.single-item').slick({
+    dots: true
+  });
+});
+
+
+(function($){
+  $(function() {
+    $('.menu__icon').on('click', function() {
+      $(this).closest('.menu').toggleClass('menu_state_open');
+    });
+  });
+})(jQuery);
